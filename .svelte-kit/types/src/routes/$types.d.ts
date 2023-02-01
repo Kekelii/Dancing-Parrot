8c14f9cacc0efce50a949eba7e76e5d125e2ac16
courage <courageagbenyegah234@gmail.com>
2023-02-01 22:24:18 +0000

    scaffold ready

--- .svelte-kit/types/src/routes/$types.d.ts
+++ .svelte-kit/types/src/routes/$types.d.ts
@@ -8,12 +8,9 @@ export type RequiredKeys<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } 
 type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Partial<Pick<App.PageData, keyof T & keyof App.PageData>> & Record<string, any>>
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
-type PageParentData = EnsureDefined<LayoutData>;
-type LayoutRouteId = RouteId | "/" | null
+type LayoutRouteId = RouteId | null
 type LayoutParams = RouteParams & {  }
 type LayoutParentData = EnsureDefined<{}>;
 
-export type PageServerData = null;
-export type PageData = Expand<PageParentData>;
 export type LayoutServerData = null;
 export type LayoutData = Expand<LayoutParentData>;
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+  </script>
+
+<div class="h-screen w-screen layout flex items-center justify-center">
+ <img src="/logo/logo.png" alt="logo" class="h-16 w-16"/>
+</div>
+<style>
+    .layout{
+        background: linear-gradient(180deg, #FFFFFF 0%, #FFFACB 100%);
+    }
+</style>
