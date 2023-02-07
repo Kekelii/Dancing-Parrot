31f70ee54c95b0ec6ddad293e9d33ac2bab76b21
courage <courageagbenyegah234@gmail.com>
2023-02-07 10:45:49 +0000

    mackup done

--- src/lib/episodes-body/components/seasons.svelte
+++ src/lib/episodes-body/components/seasons.svelte
@@ -5,7 +5,7 @@
 </script>
 <div class=" mt-14">
     <p class="uppercase text-4xl font-light ml-5 leading-10 tracking-wide inline-block ">Season {seasonnumber}</p>
-    <div class="episodes mt-5 flex flex-wrap  md:gap-0 gap-2 p-1">
+    <div class="episodes mt-5 flex flex-wrap  md:gap-0 gap-2 p-1 justify-around">
         <EpisodeContainer/>
         <EpisodeContainer/>
         <EpisodeContainer/>
