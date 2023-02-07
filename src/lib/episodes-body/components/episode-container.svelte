eb6e5866383d1032b531014ba10a1201afb91898
courage <courageagbenyegah234@gmail.com>
2023-02-07 19:43:27 +0000

    mackup done

--- src/lib/episodes-body/components/episode-container.svelte
+++ src/lib/episodes-body/components/episode-container.svelte
@@ -2,18 +2,18 @@
     export let episodenumber=15;
     export let date= '25th november 2025'
 </script>
-<div class="container p-0 md:w-48 md:h-40 md:m-5 w-28 h-24 rounded-md shrink-0">
+<div class="episode  md:w-48 md:h-40 md:m-5 w-44  h-32 rounded-md shrink-0">
     <div class="info mt-5 md:mt-12">
-        <p class='text-right w-4/5'>Episode Name</p>
-        <p class='uppercase text-sm text-right w-4/5 font-light leading-4 tracking-wide  inline-block '>episode {episodenumber}</p>
-        <div class=" flex text-base uppercasetext-right  w-full justify-around md:w-4/5 md:justify-end">
+        <p class='text-right inline-block ml-2'>Episode Name</p>
+        <p class='uppercase text-sm inline-block pl-2 w-4/5 font-light leading-4 tracking-wide'>episode {episodenumber}</p>
+        <div class=" flex  text-base uppercasetext-right  pl-2 w-full md:w-4/5 md:justify-start">
             <img src="/generalIcons/calender.svg" class='w-4 h-4 mr-2' alt="calandericon"/>
-            <p class="date p-0">{date}</p>
+            <p class="date p-0 inline-block     ">{date}</p>
         </div>
     </div>
 </div>
 <style>
-    .container{
+    .episode{
         background: #FFF59F;
         font-family: bison;
     }
