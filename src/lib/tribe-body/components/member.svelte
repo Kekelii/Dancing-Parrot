e93ea466d15168160525545e8f701e658093127a
courage <courageagbenyegah234@gmail.com>
2023-02-15 10:33:04 +0000

    styling test

--- src/lib/tribe-body/components/member.svelte
+++ src/lib/tribe-body/components/member.svelte
@@ -12,14 +12,14 @@
    
 </script>
 
-<div class="flex flex-col justify-around  h-72 w-2/5 p-o  md:w-72 md:h-96 md:p-5 md:mt-10 uppercase mb-3  md:shadow-lg"> 
+<div class="flex flex-col justify-around rounded-sm shadow-lg h-72 w-2/5  pb-2  md:w-72 md:h-96 md:p-5 md:mt-10 uppercase mb-3  md:shadow-lg md:rounded-xl"> 
     <div class=" h-4/5  shrink-0">
         <img src="tribemembers/{memberData.image}" alt="{memberData.name}'s' profile" class="object-center h-full w-full">
     </div>
     <div class="w-full ">
         <p class="w-full  text-sm md:text-xl  text-center">{memberData.name}</p>
     </div>
-    <div class="socials mt-2 flex justify-start gap-5   items-center md:mt-5 ">
+    <div class="socials mt-2 flex justify-start gap-5 m-2  items-center md:mt-5 ">
        <a  href={memberData.instagram} class:hidden={memberData.instagram ==='false'}><img src="/generalIcons/instagram.svg" alt="instagram"></a> 
        <a href={memberData.twitter} class:hidden={memberData.twitter ==='false'}><img src="/generalIcons/twitter.svg" alt="twitter"></a>
         <a href={memberData.facebook} class:hidden={memberData.facebook ==='false'}><img src="/generalIcons/facebook.svg" alt="facebook"></a>
