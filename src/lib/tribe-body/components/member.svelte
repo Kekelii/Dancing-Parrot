11479bc716d883994ed9207173f392301cdd46be
courage <courageagbenyegah234@gmail.com>
2023-02-15 11:51:23 +0000

    styling test

--- src/lib/tribe-body/components/member.svelte
+++ src/lib/tribe-body/components/member.svelte
@@ -7,9 +7,6 @@
         linkedIn:'',
         image:''
     }
-
-   
-   
 </script>
 
 <div class="flex flex-col justify-around rounded-sm shadow-lg h-72 w-2/5  pb-2  md:w-72 md:h-96 md:p-5 md:mt-10 uppercase mb-3  md:shadow-lg md:rounded-xl"> 
@@ -17,7 +14,7 @@
         <img src="tribemembers/{memberData.image}" alt="{memberData.name}'s' profile" class="object-center h-full w-full">
     </div>
    
-        <p class="w-full flex bg-slate-400 p-0 text-sm md:text-xl  md:text-center">{memberData.name}</p>
+        <span class="w-full  p-0 text-sm md:text-xl  md:text-center text-center">{memberData.name}</span>
   
     <div class="socials mt-2 flex justify-start gap-5   items-center md:mt-5 ">
        <a  href={memberData.instagram} class:hidden={memberData.instagram ==='false'}><img src="/generalIcons/instagram.svg" alt="instagram"></a> 
@@ -28,7 +25,8 @@
 </div>
 
 <style>
-   p{
-        font-family: bison;  
-    }
+   span{
+    font-family: 'Bison', sans-serif;
+                                                
+       }
 </style>
