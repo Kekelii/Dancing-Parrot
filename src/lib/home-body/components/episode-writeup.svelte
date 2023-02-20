afcd4350dc6cc1b343d04c2f16565fd4725849f1
courage <courageagbenyegah234@gmail.com>
2023-02-20 12:47:25 +0000

    transition complete

--- src/lib/home-body/components/episode-writeup.svelte
+++ src/lib/home-body/components/episode-writeup.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { onDestroy } from 'svelte';
   import { fade } from 'svelte/transition';
-     import {episode} from '../../episodeStores'
+  import {episode} from '../../episodeStores'
     
     export let writeup ="Lorem ipsum dolor sit amet, consectetur adipiscing elit  Ut ut aliquet purus. Pellentesque lorem lacus, commodo eu scelerisque a, aliquet et ligula. Aliquam ut interdum nulla.Phasellus non congue ante. Sed rhoncus a purus sit amet dictum. Vivamus et nibh neque. Aliquam feugiat quis elit ac congue."
-    episode.subscribe(value=>writeup = value.description)
+   const unsubscribe=  episode.subscribe(value=>writeup = value.description)
 
+onDestroy(unsubscribe)
 </script>
 <div in:fade="{{duration:1000}}" class=" subpixel-antialiased scrollbar-hidden font-medium w-5/5  h-80 ml-5 mr-5 text-xs  md:text-sm  tracking-wider leading-relaxed text-left  mt-2 overflow-y-auto md:w-5/5  md:h-44 ">
     {writeup}
