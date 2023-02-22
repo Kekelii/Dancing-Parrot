e81d076f0a1701d189c97e68673bfdded2c18eff
courage <courageagbenyegah234@gmail.com>
2023-02-22 20:28:15 +0000

    kasa team changes

--- src/lib/home-body/components/episode-number.svelte
+++ src/lib/home-body/components/episode-number.svelte
@@ -5,7 +5,7 @@
      let EpisodeNumber = 0;
      episode.subscribe(value=>EpisodeNumber = value.number)
 </script>
-<p in:fly='{{x:200,duration:900}}' class="pl-7 text-2xl uppercase text-red-600">Latest Episode</p>
+<p in:fly='{{x:200,duration:900}}' id='latest_episode' class="pl-7 text-2xl uppercase ">Latest Episode</p>
 <p in:fade="{{duration:1000}}" class=" md:shrink-0 inline-block pl-7 text-2xl uppercase tracking-wide pt-1">EPISODE {EpisodeNumber}</p>
 
 
@@ -13,5 +13,8 @@
     p{
         font-family: bison;
       
+    }
+    #latest_episode{
+        color:#c08e0b;
     }
 </style>
