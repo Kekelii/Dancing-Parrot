e9d18b7573fc6c183b99b5d5593f2afd9b1999d5
courage <courageagbenyegah234@gmail.com>
2023-02-28 23:50:00 +0000

    adding catchy text to banner

--- src/lib/home-body/components/banner.svelte
+++ src/lib/home-body/components/banner.svelte
@@ -7,7 +7,7 @@
 <div in:fade="{{duration:2000,delay:200}}" class='w-2/2  md:w-1/2 md:shrink-0 md:flex md:flex-col md:items-center'>
 <p class="w-4/5 m-auto text-sm md:text-lg md:w-full">A livestream podcast where very little is 'off topic'. #unscripted #the_Tribe #vibes</p> 
 <p class="w-4/5 m-auto text-xs md:text-sm mt-2 md:mt-0  mb-2 md:w-full bannerwriteup">Tune in to our weekly livestreams on Facebook and Youtube on Sundays at 7pm GMT.<br>
- <span class="text-black italic cursor-pointer"><a href="https://anchor.fm/kasathetribe">And find</a> </span> us in all podcast directories in the links below
+ <span class="text-black italic cursor-pointer"><a href="https://anchor.fm/kasathetribe">And find</a> </span> us in all podcast directories 
 </p>
   <Splide class="hidden md:block"  options={{rewind: true,gap:'2rem',autoplay:true,pauseOnHover:true}}>
     <SplideSlide>
